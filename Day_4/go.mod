1496f758bb3084899ec72898f6dc75038a94d717
Titanium-ly <ly610855878@icloud.com>
2022-02-22 21:23:50 +0800

    Keep hungry

---
+++ Day_4/go.mod
@@ -0,0 +1,3 @@
+module github.com/aceld/moudles_test
+
+go 1.14
---
+++ Golang_IM_System/server.go
@@ -0,0 +1,88 @@
+package main
+
+import (
+	"fmt"
+	"net"
+	"sync"
+)
+
+type Server struct {
+	Ip   string
+	Port int
+	// 在线的用户列表
+	OnlineMap map[string]*User
+	mapLock   sync.RWMutex //RWMutex（读写锁）
+
+	//消息广播的channel
+	Message chan string
+}
+
+// NewServer NewsServer 创建一个 server 对象，返回用指针接收
+func NewServer(ip string, port int) *Server {
+	// new 一个server 对象，把&Server地址指针给了server 然后让*Server 接收
+	server := &Server{
+		Ip:        ip,
+		Port:      port,
+		OnlineMap: make(map[string]*User),
+		Message:   make(chan string),
+	}
+	return server
+}
+
+// Start 启动服务 相当于 Server类的一个成员方法
+func (this *Server) Start() {
+	// socket listen
+	listener, err := net.Listen("tcp", fmt.Sprintf("%s:%d", this.Ip, this.Port))
+	if err != nil {
+		fmt.Println("net.Listen err:", err)
+		return
+	}
+
+	// close listener socket
+	defer listener.Close()
+
+	go this.ListenServerMessage()
+
+	// accept 如果接收到连接，就去处理业务 --> this.Handler(conn)
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			fmt.Println("listener accept:", err)
+			continue
+		}
+		go this.Handler(conn)
+	}
+
+}
+
+// Handler 处理业务
+func (this *Server) Handler(conn net.Conn) {
+	// 用户上线，将用户加入到OnlineMap
+	user := NewUser(conn)
+	this.mapLock.Lock()
+	this.OnlineMap[user.Name] = user
+	this.mapLock.Unlock()
+	// 广播当前消息
+	this.BroadCast(user, "用户上线了")
+
+	// 发送完消息，先让当前 Handler 阻塞，否则他的子goroutine 都得GG
+	select {}
+}
+
+// BroadCast 广播方法
+func (this *Server) BroadCast(user *User, msg string) {
+	sendMsg := "[" + user.Addr + "]" + user.Name + ":" + msg
+	this.Message <- sendMsg
+}
+
+// ListenServerMessage 持续监听
+func (this *Server) ListenServerMessage() {
+	for {
+		msg := <-this.Message
+		this.mapLock.Lock()
+		for _, cli := range this.OnlineMap {
+			cli.C <- msg
+		}
+		this.mapLock.Unlock()
+	}
+}
