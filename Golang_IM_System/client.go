32f9a9f964949c59fd1644d378ea781fb92dc55a
Titanium-ly <ly610855878@icloud.com>
2022-02-27 20:45:22 +0800

    增加私聊功能

--- Golang_IM_System/client.go
+++ Golang_IM_System/client.go
@@ -8,6 +8,7 @@ import (
 	"os"
 )
 
+// Client 客户端类
 type Client struct {
 	ServerIp   string
 	ServerPort int
@@ -16,6 +17,7 @@ type Client struct {
 	flag       int
 }
 
+// NewClient 创建新的客户端对象
 func NewClient(serverIp string, serverPort int) *Client {
 	client := &Client{
 		ServerIp:   serverIp,
@@ -31,6 +33,7 @@ func NewClient(serverIp string, serverPort int) *Client {
 	return client
 }
 
+//开始菜单
 func (client *Client) menu() bool {
 	var flag int
 	fmt.Println("1.共聊")
@@ -48,26 +51,69 @@ func (client *Client) menu() bool {
 	}
 }
 
-func (client *Client) Run() {
-	for client.flag != 0 {
-		for client.menu() != true {
+// PublicChat 公共聊天
+func (client *Client) PublicChat() {
+	var chatMsg string
+	fmt.Println(">>>>请输入聊天内容， exit退出")
+	fmt.Scanln(&chatMsg)
+
+	for chatMsg != "exit" {
+		if len(chatMsg) != 0 {
+			sendMsg := chatMsg + "\n"
+			_, err := client.conn.Write([]byte(sendMsg))
+			if err != nil {
+				fmt.Println("conn Write err", err)
+				break
+			}
 		}
-		switch client.flag {
-		case 1:
-			// 共聊模式
-			fmt.Println("共聊模式选择...")
-			break
-		case 2:
-			// 私聊模式
-			fmt.Println("私聊模式...")
-			break
-		case 3:
-			// 更新用户名
-			client.UpdateName()
-			break
-		}
+		chatMsg = ""
+		fmt.Println(">>>>>请输入聊天内容, exit退出")
+		fmt.Scanln(&chatMsg)
 	}
 }
+
+// SelectUsers 查询在线用户
+func (client *Client) SelectUsers() {
+	sendMsg := "who\n"
+	_, err := client.conn.Write([]byte(sendMsg))
+	if err != nil {
+		fmt.Println("conn Write err", err)
+		return
+	}
+}
+
+func (client *Client) PrivatChat() {
+	var remoteName string
+	var chatMsg string
+
+	client.SelectUsers()
+	fmt.Println(">>>>>请输入聊天对象[用户名], exit退出")
+	fmt.Scanln(&remoteName)
+
+	for remoteName != "exit" {
+		fmt.Println(">>>>>请输入发送消息内容, exit退出:")
+		fmt.Scanln(&chatMsg)
+		for chatMsg != "exit" {
+			if len(chatMsg) != 0 {
+				sendMsg := "to|" + remoteName + "|" + chatMsg + "\n"
+				_, err := client.conn.Write([]byte(sendMsg))
+				if err != nil {
+					fmt.Println("conn Write err:", err)
+					break
+				}
+			}
+			chatMsg = ""
+			fmt.Println("请输入消息内容, exit退出:")
+			fmt.Scanln(&chatMsg)
+		}
+		client.SelectUsers()
+		fmt.Println(">>>>> 请输入消息内容,exit退出：")
+		fmt.Scanln(&remoteName)
+	}
+
+}
+
+// UpdateName 更新用户名
 func (client *Client) UpdateName() bool {
 	fmt.Println(">>>>>请输入用户名:")
 	fmt.Scanln(&client.Name)
@@ -84,6 +130,27 @@ func (client *Client) UpdateName() bool {
 func (client *Client) DealResponse() {
 	// 一旦client.conn 有数据，就直接copy 到 stdout 标准输出上，永久阻塞监听
 	io.Copy(os.Stdout, client.conn)
+}
+
+func (client *Client) Run() {
+	for client.flag != 0 {
+		for client.menu() != true {
+		}
+		switch client.flag {
+		case 1:
+			// 共聊模式
+			client.PublicChat()
+			break
+		case 2:
+			// 私聊模式
+			client.PrivatChat()
+			break
+		case 3:
+			// 更新用户名
+			client.UpdateName()
+			break
+		}
+	}
 }
 
 var serverIp string
